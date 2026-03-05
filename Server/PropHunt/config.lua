8e28ea5eaf0f4af74ab1acf10338bf8ccadc782f
Ashmaker000 <8276382+ashmaker000@users.noreply.github.com>
2026-03-05 09:10:25 +0000

    Fix formatting in propPool list in config.lua

--- Server/PropHunt/config.lua
+++ Server/PropHunt/config.lua
@@ -90,12 +90,12 @@ M.defaults = {
 
   -- Prop pool (official internal names)
   propPool = {
-    "anticut", "barrels", "ball", "barrier", "barrier_plastic", "blockwall", "bollard",
+    "anticut", "barrels", "ball", "barrier", "barrier_plastic", "blockwall",
     "caravan", "chair", "cones", "couch", "crowdbarrier", "delineator", "engine_props",
-    "flail", "flipramp", "fridge", "gate", "haybale", "kickplate", "logs", "marble_block",
+    "flail", "flipramp", "fridge", "gate", "haybale", "logs", "marble_block",
     "mattress", "metal_box", "metal_ramp", "piano", "porta_potty", "rallyflags", "rallysigns",
     "roadsigns", "rock_pile", "rocks", "sawhorse", "shipping_container", "spikestrip",
-    "steel_coil", "trampoline", "tirewall", "trafficbarrel", "trashbin", "tube", "tv",
+    "steel_coil", "trampoline", "tirewall", "trafficbarrel", "trashbin", "tv",
     "wall", "woodcrate", "woodplanks"
   }
 }
